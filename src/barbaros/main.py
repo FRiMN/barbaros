682972ea804b9b67d3587cae9ee7bb61ebcf6731
Nik Volkov <freezemandix@ya.ru>
2025-08-16 07:55:29 +0700

    working qt tray

--- src/barbaros/main.py
+++ src/barbaros/main.py
@@ -1,40 +1,42 @@
+from importlib.resources import path
+
 from PySide6.QtWidgets import QApplication, QSystemTrayIcon, QMenu
 from PySide6.QtGui import QIcon, QAction
+
+
+class App(QApplication):
+    def __init__(self, args, name: str):
+        super().__init__(args)
+        self.setApplicationDisplayName(name)
+
+        self.tray = TrayIcon(self)
 
 
 class TrayIcon:
     def __init__(self, app: QApplication):
         self.app = app
-        icon_img = QIcon("/usr/share/icons/desktop-base/128x128/emblems/emblem-debian-symbolic.png")
-        self.icon = QSystemTrayIcon(icon_img)
+
+        with path('barbaros.resources.icons', 'icon3.png') as icon_path:
+            icon_img = QIcon(str(icon_path.absolute()))
+        self.icon = QSystemTrayIcon(icon_img, toolTip=app.applicationDisplayName())
 
         if not self.icon.isSystemTrayAvailable():
             raise Exception("System tray is not available")
 
-        # Create context menu
-        self.menu = QMenu()
-        exit_action = QAction("Exit")
-        exit_action.triggered.connect(self.app.quit)
-        self.menu.addAction(exit_action)
-
+        self.menu = self.build_menu()
         self.icon.setContextMenu(self.menu)
 
         self.icon.setVisible(True)
 
+    def build_menu(self):
+        menu = QMenu()
 
-def get_tray():
-    tray = QSystemTrayIcon(parent=app)
-    tray.setIcon(QIcon("/usr/share/icons/desktop-base/128x128/emblems/emblem-debian-symbolic.png"))  
+        # Parent is important in this place
+        exit_action = QAction("Exit", parent=self.icon)
+        exit_action.triggered.connect(self.app.quit)
+        menu.addAction(exit_action)
 
-    menu = QMenu()
-    exit_action = QAction("Exit")
-    exit_action.triggered.connect(app.quit)
-    menu.addAction(exit_action)
-
-    tray.setContextMenu(menu)
-
-    tray.setVisible(True)
-    return tray, menu
+        return menu
 
 
 def main():
@@ -45,10 +47,7 @@ def main():
         # Allow app to be terminated with Ctrl+C
         signal.signal(signal.SIGINT, signal.SIG_DFL)
 
-        app = QApplication(sys.argv)
-        # tray_icon = TrayIcon(app)
-        tray, menu = get_tray()
-
+        app = App(sys.argv, "Barbaros")
         sys.exit(app.exec())
 
     except Exception as e:
