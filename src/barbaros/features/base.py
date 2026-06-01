414728499e01bc678fe0f535a802dc826c179b37
Nik Volkov <freezemandix@ya.ru>
2026-06-01 14:41:10 +0700

    Add type hints for AbstractFeature parent window reference

--- src/barbaros/features/base.py
+++ src/barbaros/features/base.py
@@ -1,16 +1,22 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING
+
 from PySide6.QtCore import QObject
-from PySide6.QtWidgets import QMainWindow, QBoxLayout
+from PySide6.QtWidgets import QBoxLayout
 
 from barbaros.common import SettingsProxy
+
+if TYPE_CHECKING:
+    from barbaros.main_window import MainWindow
 
 
 class AbstractFeature(QObject):
     tab_name: str
-    parent: QMainWindow
+    parent: MainWindow
     settings: SettingsProxy
     settings_key_prefix: str
 
-    def __init__(self, parent: QMainWindow):
+    def __init__(self, parent: MainWindow):
         super().__init__()
         self.parent = parent
         self.settings = SettingsProxy(parent.app.settings, self.settings_key_prefix)
